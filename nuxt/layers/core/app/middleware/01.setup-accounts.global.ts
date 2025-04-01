c9862812a3b1a1e32ba836214586cc8af66d4532
deetz99 <73151365+deetz99@users.noreply.github.com>
2025-04-01 10:09:24 -0700

    Account id url query (#50)
    * assign account if accountid in url query
    
    * version

--- nuxt/layers/core/app/middleware/01.setup-accounts.global.ts
+++ nuxt/layers/core/app/middleware/01.setup-accounts.global.ts
@@ -1,8 +1,13 @@
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtRouteMiddleware(async (to) => {
   if (import.meta.client) { // only run on client
     const { isAuthenticated } = useKeycloak()
     if (isAuthenticated.value) {
-      await useConnectAccountStore().initAccountStore()
+      const accountStore = useConnectAccountStore()
+      await accountStore.initAccountStore()
+
+      if (to.query.accountid) {
+        accountStore.switchCurrentAccount(parseInt(to.query.accountid as string))
+      }
     }
   }
 })
