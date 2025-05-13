74c02d5008e2cb7d6f73d06eefc34a5655a010cf
Travis Semple <travis8814@gmail.com>
2025-05-13 09:46:44 -0700

    Add in x-apikey for auth-api if it exists

--- nuxt/layers/core/app/plugins/auth-api.ts
+++ nuxt/layers/core/app/plugins/auth-api.ts
@@ -1,5 +1,6 @@
 export default defineNuxtPlugin(() => {
   const authApiUrl = useRuntimeConfig().public.authApiURL
+  const authApiKey = useRuntimeConfig().public.authApiKey
   const errorRedirectPath = useAppConfig().connect.core.plugin.authApi.errorRedirect[401]
   const { $keycloak } = useNuxtApp()
   const localePath = useLocalePath()
@@ -8,13 +9,24 @@ export default defineNuxtPlugin(() => {
     baseURL: authApiUrl,
     onRequest ({ options }) {
       const headers = options.headers ||= {} as Headers
+      const bearerToken = `Bearer ${$keycloak.token}`
       if (Array.isArray(headers)) {
-        headers.push(['Authorization', `Bearer ${$keycloak.token}`])
+        headers.push(['Authorization', bearerToken])
+        if (authApiKey) {
+          headers.push(['x-apikey', authApiKey])
+        }
       } else if (headers instanceof Headers) {
-        headers.set('Authorization', `Bearer ${$keycloak.token}`)
+        headers.set('Authorization', bearerToken)
+        if (authApiKey) {
+          headers.set('x-apikey', authApiKey)
+        }
       } else {
         // @ts-expect-error - 'Authorization' doesnt exist on type Headers
-        headers.Authorization = `Bearer ${$keycloak.token}`
+        headers.Authorization = bearerToken
+        if (authApiKey) {
+          // @ts-expect-error - 'Authorization' doesnt exist on type Headers
+          headers['x-apikey'] = authApiKey
+        }
       }
     },
     async onResponseError ({ response }) {
